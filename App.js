946d1eac674f8976a9dd75e9e95c027bbdb83aaa
valtikamore <77681004+valtikamore@users.noreply.github.com>
2021-06-06 23:54:26 +0300

    1- image / button / callbacks / Alert-prompt / Touchable / styles

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button, Alert, Image, TouchableHighlight, TouchableWithoutFeedback} from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const handlePress = () => Alert.alert('0101010', 'main message', [
+        {test: 'button text', onPress: () => console.log('tes')},
+        {test: 'button text', onPress: () => console.log('eeee')},
+    ])
+    const handlePress2 = () => Alert.prompt('0101010', 'main message', [
+        {test: 'button text', onPress: () => console.log('tes')},
+        {test: 'button text', onPress: () => console.log('eeee')},
+    ])
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>hello {'\n'}friend</Text>
+            <Text style={styles.text}>hello {'\n'}friend</Text>
+            <Button title="Press me" onPress={handlePress}/>
+            <Button title="Press me" onPress={handlePress2} style={{marginBottom: 20, display: 'block'}}/>
+            <TouchableHighlight onPress={handlePress} style={{marginBottom: 20}}>
+                <Image blurRadius={2} source={{
+                    width: 200,
+                    height: 200,
+                    uri: 'https://picsum.photos/200/300'
+                }}/>
+            </TouchableHighlight>
+            <TouchableWithoutFeedback onPress={handlePress}>
+                <Image blurRadius={2} source={{
+                    width: 200,
+                    height: 200,
+                    uri: 'https://picsum.photos/200/300'
+                }}/>
+            </TouchableWithoutFeedback>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        padding: 20,
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'black',
+    },
+    text: {
+        color: 'white'
+    }
 });
